41ea918cc1dec9e6cbe3f38f75bea543437abf8a
rarora <rarora@mathworks.com>
2026-03-31 14:06:29 +0530

    2nd Fix to Pipeline

--- Scripts/GenerateYML.m
+++ Scripts/GenerateYML.m
@@ -3,6 +3,6 @@ GitHubOptions.MatlabInstallationLocation = '"C:\\Program Files\\MATLAB\\R2025b\\
 
 
 
-GitHubOptions.MatlabLaunchCmd = '"C:\\Program Files\\MATLAB\\R2025b\\bin\\matlab.exe"';
+%GitHubOptions.MatlabLaunchCmd = '"C:\\Program Files\\MATLAB\\R2025b\\bin\\matlab.exe"';
 GitHubOptions.PipelineArchitecture = "SerialStages";
 padv.pipeline.generatePipeline(GitHubOptions)
