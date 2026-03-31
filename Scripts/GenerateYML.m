c2c65a9bbb56f48c8c1be488ede7ff053de6e593
rarora <rarora@mathworks.com>
2026-03-31 13:18:26 +0530

    Fix Pipeline

--- Scripts/GenerateYML.m
+++ Scripts/GenerateYML.m
@@ -1,8 +1,8 @@
-GitHubOptions = padv.pipeline.GitHubOptions(GeneratorVersion=1)
-GitHubOptions.MatlabInstallationLocation = '"C:\\Program Files\\MATLAB\\R2025b\\bin\\"';
-GitHubOptions.TemplatePath = "./.github/workflows/simulink_pipeline.yml";
-GitHubOptions.NetworkStoragePath = "/artifactManagement/cacheStorage";
-GitHubOptions.SupportPackageRoot = "C:\\ProgramData\\MATLAB\\SupportPackages\\R2025b";
-GitHubOptions.MatlabLaunchCmd = '"C:\\Program Files\\MATLAB\\R2025b\\bin\\matlab.exe"'
-GitHubOptions.PipelineArchitecture = "SerialStages"
+GitHubOptions = padv.pipeline.GitHubOptions(GeneratedPipelineDirectory=1)
+GitHubOptions.MatlabInstallationLocation = "/usr/local/MATLAB/R2025b/bin";
+
+
+
+GitHubOptions.MatlabLaunchCmd = "/usr/local/MATLAB/R2025b/bin/matlab.exe";
+GitHubOptions.PipelineArchitecture = "SerialStages";
 padv.pipeline.generatePipeline(GitHubOptions)
