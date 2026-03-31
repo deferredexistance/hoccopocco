004f4fcd02f10d15b42169989a13b1b160f8ce3b
rarora <rarora@mathworks.com>
2026-03-31 08:47:58 +0530

    test

--- Scripts/GenerateYML.m
+++ Scripts/GenerateYML.m
@@ -3,6 +3,6 @@ GitHubOptions.MatlabInstallationLocation = '"C:\\Program Files\\MATLAB\\R2025b\\
 GitHubOptions.TemplatePath = "./.github/workflows/simulink_pipeline.yml";
 GitHubOptions.NetworkStoragePath = "/artifactManagement/cacheStorage";
 GitHubOptions.SupportPackageRoot = "C:\\ProgramData\\MATLAB\\SupportPackages\\R2025b";
-GitHubOptions.MatlabLaunchCmd = "C:\Program Files\MATLAB\R2025b\bin\matlab.exe"
+GitHubOptions.MatlabLaunchCmd = '"C:\\Program Files\\MATLAB\\R2025b\\bin\\matlab.exe"'
 GitHubOptions.PipelineArchitecture = "SerialStages"
 padv.pipeline.generatePipeline(GitHubOptions)
