7104e0f0d4ca2e82f58ce259c7af0aa02c9cc374
rarora <rarora@mathworks.com>
2026-03-31 17:13:48 +0530

    another fix to startupcommand

--- Scripts/GenerateYML.m
+++ Scripts/GenerateYML.m
@@ -1,8 +1,10 @@
 GitHubOptions = padv.pipeline.GitHubOptions(GeneratorVersion=1)
 GitHubOptions.MatlabInstallationLocation = '"C:\\Program Files\\MATLAB\\R2025b\\bin\\"';
 
+LaunchCommandString = ['C:\\Program Files\\MATLAB\\R2025b\\bin\\matlab.exe', ...
+    ' -nodesktop -logfile'];
 
-
-%GitHubOptions.MatlabLaunchCmd = '"C:\\Program Files\\MATLAB\\R2025b\\bin\\matlab.exe"';
+GitHubOptions.MatlabLaunchCmd = LaunchCommandString;
+GitHubOptions.MatlabStartupOptions = "";
 GitHubOptions.PipelineArchitecture = "SerialStages";
 padv.pipeline.generatePipeline(GitHubOptions)
