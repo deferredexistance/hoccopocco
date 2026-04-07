ef30f2dd44cdbe574ff643399e72fda4e76692f3
rarora <rarora@mathworks.com>
2026-04-07 14:05:56 +0530

    launchcommand updated

--- Scripts/GenerateYML.m
+++ Scripts/GenerateYML.m
@@ -8,10 +8,10 @@ GitHubOptions.ArtifactServiceMode = "network";
 GitHubOptions.NetworkStoragePath = "/artifactManagement/cacheStorage";
 GitHubOptions.RelativeProjectPath = "";
 GitHubOptions.UseMatlabPlugin = false;
-LaunchCommandString = ['C:\\Program Files\\MATLAB\\R2025b\\bin\\matlab.exe', ...
-    ' -nodesktop -logfile'];
-
-GitHubOptions.MatlabLaunchCmd = LaunchCommandString;
+% LaunchCommandString = ['C:\\Program Files\\MATLAB\\R2025b\\bin\\matlab.exe', ...
+%     ' -nodesktop -logfile'];
+% 
+% GitHubOptions.MatlabLaunchCmd = LaunchCommandString;
 GitHubOptions.MatlabStartupOptions = "";
 GitHubOptions.PipelineArchitecture = "SerialStages";
 padv.pipeline.generatePipeline(GitHubOptions)
